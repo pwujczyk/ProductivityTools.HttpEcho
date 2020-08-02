1a3a9ee2d0a5b0691e60e38ab5f0b7ce1edc4199
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-08-02 21:12:10 +0200

    calls

---
+++ ProductivityTools.HttpEcho/Calls.ps1
@@ -0,0 +1,4 @@
+﻿Invoke-WebRequest -Uri http://localhost:8088/Echo/Date -UseBasicParsing
+Invoke-WebRequest -Uri http://192.168.0.51:8088/Echo/Date -UseBasicParsing
+Invoke-WebRequest -Uri http://79.184.198.78:8088/Echo/Date -UseBasicParsing
+Invoke-WebRequest -Uri http://echo.productivitytools.tech:8088/Echo/Date -UseBasicParsing
